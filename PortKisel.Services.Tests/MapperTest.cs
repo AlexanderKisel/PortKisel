487fe7626498337d2de7fc485d19564aa1493df9
AlexanderKisel <alekskisel5250@gmail.com>
2023-12-30 01:07:27 +0300

    Коммиты были украдены. Основа для тестов

---
+++ PortKisel.Services.Tests/MapperTest.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using PortKisel.Services.AutoMappers;
+using Xunit;
+
+namespace PortKisel.Services.Tests
+{
+    public class MapperTest
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void TestMap()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ServiceProfile());
+            });
+
+            config.AssertConfigurationIsValid();
+        }
+    }
+}
---
+++ PortKisel.Services.Tests/Tests/StaffServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortKisel.Services.Tests.Tests
+{
+    internal class StaffServiceTests
+    {
+    }
+}
